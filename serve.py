825fba2a6c85f2cda071782d42874e18cd394e5a
ab <ab>
2023-07-25 00:23:34 -0500

    Setup small map, basic character movement

---
+++ serve.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+
+PORT = 8000
+DIRECTORY = "dist"
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def end_headers(self):
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        super().end_headers()
+
+Handler = MyHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
