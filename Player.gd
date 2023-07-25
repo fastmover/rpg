1126f559c5d13d679fd79cfef427ae7cb4e7d3ae
ab <ab>
2023-07-25 14:22:01 -0500

    map updates, camera view updates

--- Player.gd
+++ Player.gd
@@ -1,17 +1,11 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 100
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 func get_input():
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	
 	if velocity.x < 0:
@@ -25,23 +19,11 @@ func get_input():
 	
 	elif velocity.y > 0:
 		_animated_sprite.play('walk_down')
+	
+	else:
+		_animated_sprite.stop()
+	
 
 func _physics_process(delta):
 	get_input()
-#	# Add the gravity.
-#	if not is_on_floor():
-#		velocity.y += gravity * delta
-#
-#	# Handle Jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
-#
-#	# Get the input direction and handle the movement/deceleration.
-#	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var direction = Input.get_axis("ui_left", "ui_right")
-#	if direction:
-#		velocity.x = direction * SPEED
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 A journey of sorts.
 
-
-
-
-
+---
 
 Assets:
 
 [ElvGames](https://elv-games.itch.io/) - for the Character [Sprites](https://elv-games.itch.io/free-fantasy-dreamland-sprites)
+
 [Shade](https://itch.io/profile/merchant-shade) - for the world tilemap - [Asset Link](https://merchant-shade.itch.io/16x16-puny-world)
